77d39032fd946d767df054060e85f1655eb591ea
Dmitriy Bagandov <dimon.bagandov@gmail.com>
2018-11-20 00:32:58 +0200

    Added enemy behavior

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -2,30 +2,30 @@
 
 public class EnemyMovement : MonoBehaviour {
 
-    public float speed = 3f;
+    public float speed = 130f;
 
     public CharacterController2D movement;
     public Transform groundDetection;
 
-    private bool movingRight = true;
+    private float direction = 1f;
 
 	void FixedUpdate () {
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
+		movement.Move (direction * speed * Time.deltaTime, false, false);
 
-        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, 2f);
-        if (groundInfo.collider == false)
-            Flip();
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, 1f);
+		Debug.Log (groundInfo.collider);
+		if (!groundInfo.collider)
+			FlipDirection ();
 	}
 
-    void Flip() {
-        if (movingRight)
-        {
-            transform.eulerAngles = new Vector3(0, -180, 0);
-            movingRight = false;
-        } else
-        {
-            transform.eulerAngles = new Vector3(0, 0, 0);
-            movingRight = true;
-        }
-    }
+	void FlipDirection() {
+		Debug.Log (direction);
+		if (direction == 1f) {
+			transform.eulerAngles = new Vector3 (0, -180, 0);
+			direction = -1f;
+		} else {
+			transform.eulerAngles = new Vector3 (0, 0, 0);
+			direction = 1f;
+		}
+	}
 }
