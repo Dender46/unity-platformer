6bcbefcc271615aa07bf066c2a155a06a3499af8
Dmitriy Bagandov <dimon.bagandov@gmail.com>
2018-12-14 09:29:53 +0200

    Cleaned up project

---
+++ Assets/Scripts/ResizeScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class ResizeScript : MonoBehaviour {
+
+	public float scaleX;
+	public float scaleY;
+	public float cubeSize;
+
+	void Update () {
+		transform.localScale = new Vector3 ((scaleX + 0.8f) * cubeSize, (scaleY + 0.8f) * cubeSize, 0.0f);
+	}
+}
