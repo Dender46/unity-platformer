1a779f7c06cfac8b02224eb23c0c8a8200ded7c0
Dmitriy Bagandov <dimon.bagandov@gmail.com>
2018-12-15 12:41:35 +0200

    Resize script

--- Assets/Scripts/ResizeScript.cs
+++ Assets/Scripts/ResizeScript.cs
@@ -6,9 +6,12 @@ public class ResizeScript : MonoBehaviour {
 
 	public float scaleX;
 	public float scaleY;
+	public float factorX;
+	public float factorY;
+
 	public float cubeSize;
 
 	void Update () {
-		transform.localScale = new Vector3 ((scaleX + 0.8f) * cubeSize, (scaleY + 0.8f) * cubeSize, 0.0f);
+		transform.localScale = new Vector3 ((scaleX + factorX) * cubeSize, (scaleY + factorY) * cubeSize, 0.0f);
 	}
 }
